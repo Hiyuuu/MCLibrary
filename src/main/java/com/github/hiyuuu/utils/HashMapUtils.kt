3eaae5b2633eec01210815a6a5a4e7deee2b3ce3
Hiyuuu <Hiyuuu@github.com>
2024-01-10 08:28:30 +0900

    added HashMapUtils.kt

---
+++ src/main/java/com/github/hiyuuu/utils/HashMapUtils.kt
@@ -0,0 +1,16 @@
+package com.github.hiyuuu.utils
+
+/**
+ * ハッシュマップを使用上で便利するクラス
+ */
+interface HashMapUtils {
+
+    /**
+     * ハッシュマップのキーを最新に置き換える
+     * @param newKey 新しいキー
+     * @param conditions 新しいキーに置き換える条件式
+     */
+    fun <K, V> HashMap<K, V>.replaceHashMapKey(newKey: K, conditions: (K) -> Boolean)
+            = this.keys.filter(conditions).forEach { v -> this.remove(v)?.let { this[newKey] = it } }
+    
+}
