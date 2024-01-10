28e5b83bd7de758e362c34c440d68db346d2e5c7
Hiyuuu <hiih12242408@gmail.com>
2024-01-11 06:22:21 +0900

    supported replace values of HashMapUtils.kt

--- src/main/java/com/github/hiyuuu/utils/HashMapUtils.kt
+++ src/main/java/com/github/hiyuuu/utils/HashMapUtils.kt
@@ -8,8 +8,11 @@ interface HashMapUtils {
     /**
      * ハッシュマップのキーを最新に置き換える
      * @param newKey 新しいキー
-     * @param conditions 新しいキーに置き換える条件式
+     * @param keyCondition 新しいキーに置き換える条件式
      */
-    fun <K, V> HashMap<K, V>.replaceHashMapKey(newKey: K, conditions: (K) -> Boolean)
-            = this.keys.filter(conditions).forEach { v -> this.remove(v)?.let { this[newKey] = it } }
+    fun <K, V> HashMap<K, V>.replaceHashMapKey(newKey: K, keyCondition: (K) -> Boolean, valueCondition: (V) -> V)
+        = this.forEach { k, v ->
+            val value = valueCondition.invoke(v)
+            if (keyCondition.invoke(k)) this[newKey] = value else this[k] = value
+        }
 }
